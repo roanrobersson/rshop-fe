416baff6e430a8d3148dc7d460c3680e187a7fc4
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2021-10-21 16:30:06 -0300

    Change theme h1 and h2 typography

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,32 +1,33 @@
 import { createTheme } from '@mui/material/styles';
 
 const customThemeFragment = {
-	palette: {
-		primary: {
-			contrastText: '#263238',
-			dark: '#c95382',
-			light:'#ffb6e3',
-			main: '#ff84b1' 
-		},
-		secondary: {
-			contrastText: '#263238',
-			dark: '#ab003c',
-			light:'#f73378',
-			main: '#f50057' 
-		},
-		text: {
-			disabled: '#a8adaf',
-			primary: '#263238',
-			secondary:'#1c2429',
-		}
-	},
-	typography: {
-    fontFamily: [
-      '"Open Sans"',
-      '"Helvetica"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
+  palette: {
+    primary: {
+      contrastText: '#FFFFFF',
+      dark: '#c95382',
+      light: '#ffb6e3',
+      main: '#ff84b1',
+    },
+    secondary: {
+      contrastText: '#FFFFFF',
+      dark: '#ab003c',
+      light: '#f73378',
+      main: '#f50057',
+    },
+    text: {
+      disabled: '#a8adaf',
+      primary: '#263238',
+      secondary: '#1c2429',
+    },
+  },
+  typography: {
+    fontFamily: ['"Open Sans"', '"Helvetica"', 'Arial', 'sans-serif'].join(','),
+    h1: {
+      fontWeight: 500,
+    },
+    h2: {
+      fontWeight: 500,
+    },
   },
 };
 
