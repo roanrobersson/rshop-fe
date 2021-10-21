556252934284696ebcf2b63a9d4d35f8f1e79d61
Roan Robersson de Oliveira <roan.oliveira@compasso.com.br>
2021-10-21 16:33:45 -0300

    Add NotFound page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import { Container, Box, Typography } from '@mui/material';
+
+const NotFound = () => {
+  return (
+    <Container sx={{ pt: 20, pb: 20 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant='h2'>Erro 404</Typography>
+        <Typography variant='h2'>Essa página não existe :(</Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default NotFound;
